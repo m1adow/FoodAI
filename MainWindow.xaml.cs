f5989a4f8353bcb8aa2b9e4abe9cc911672ab31b
Valentin <Vell0505189970sl@gmail.com>
2022-06-04 12:26:23 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace FoodAI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new();
+            dialog.Filter = "Image files (*.png; *.jpg)|*.png;*.jpg;*.jpeg|All files (*.*)|*.*";
+
+            if (dialog.ShowDialog() is true)
+            {
+                string fileName = dialog.FileName;
+            }
+        }
+    }
+}
